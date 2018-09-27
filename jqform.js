53a0a29f9a75c0fa86611df5bd5ba50ff3fbb905
SnehaWaghPatil <snehapatil1604@gmail.com>
2018-09-27 18:27:27 +0530

    first commit

---
+++ jqform.js
@@ -0,0 +1,51 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet2.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> 
+</script>
+<script>
+
+
+$(document).ready(function() {
+	$("input").focus(function(){
+        $(this).css("background-color", "#cccccc");
+    });
+    $("input").blur(function(){
+        $(this).css("background-color", "#ffffff");
+    });
+ 
+  $('#first_form').submit(function(e) {
+    e.preventDefault();
+    var first_name = $('#first_name').val();
+    var last_name = $('#last_name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+	var age = $(#age).val();
+	var dob = $(#dob).val();
+	var gender = $(#gender).val();	
+    $(".error").remove();
+ 
+    if (first_name.length < 1) {
+      $('#first_name').after('<span class="error">This field is required</span>');
+    }
+    if (last_name.length < 1) {
+      $('#last_name').after('<span class="error">This field is required</span>');
+    }
+    if (email.length < 1) {
+      $('#email').after('<span class="error">This field is required</span>');
+    } else {
+      var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+      var validEmail = regEx.test(email);
+      if (!validEmail) {
+        $('#email').after('<span class="error">Enter a valid email</span>');
+      }
+    }
+    if (password.length < 8) {
+      $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+    }
+  });
+ 
+});
+</script>
+
+</html>
